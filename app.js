c3aa350fae4e51aafa614a67cdcee9609b64437b
Umar khan <125694026+Umarkhan0@users.noreply.github.com>
2023-07-11 02:24:04 +0500

    Responsive nav.

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())       
-           .then(res => {
-            for(var i = 0; i < res.length; i++ ){
-                for(getItems in res[i])
-                console.log(res[i].price)
-                document.getElementById("cardsPrint").innerHTML += `
-                <div class="card" style="width: 18rem;">
-        <img " src="${res[i].image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">${res[i].price}</a>
-        </div>
-      </div>`
-            }
-           })
-            
-            
+let navbar = document.querySelector(".display");
+let mainNav = document.querySelector(".main-nav");
+let input = document.querySelector(".input");
 
+mainNav.style.height = "80px"
+console.log(mainNav.style.height)
+
+navbar.addEventListener("click", () => {
+    if (mainNav.style.height == "80px") {
+        mainNav.style.height = "190px";
+        input.style.display = "block"
+mainNav.style.transition = "0.5s"
+        
+    }
+
+    else if (mainNav.style.height !== "80px") {
+        let mainNav = document.querySelector(".main-nav");
+        mainNav.style.height = "10px"
+        console.log(mainNav.style.height)
+        mainNav.style.height = "80px";
+        input.style.display = "none"
+mainNav.style.transition = "0.5s"
+    }
+});
